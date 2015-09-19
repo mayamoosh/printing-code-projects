5f7dc93b4a196ec21e7ee21c02d2547defd5f152
mayamoosh <mmt440@nyu.edu>
2015-09-19 09:55:09 -0400

    first assignment

--- ice-cream-by-moosh/sketch.js
+++ ice-cream-by-moosh/sketch.js
@@ -1,15 +1,36 @@
 var r = new Rune( {
 	container: "body" ,
-	width: 500,
-	height: 400
+	width: 600,
+	height: 600
 });
 
 r.rect(0, 0, r.width+30, r.height/2 +30).fill(0,0,0);
 
-r.ellipse(r.width/2, r.height/2, 125, 125).fill(255,255,255).stroke(255,255,255);
 
-r.triangle(r.width/2 - 55, r.height/2 + 30.5, r.width/2 + 55, r.height/2 + 30.5, r.width/2 , r.height/2 +180).fill(0,0,0);
+r.ellipse(r.width/2 - 30, r.height/2 - 40, 100, 100)
+.fill(255,255,255)
+.stroke(0,0,0)
+.strokeWidth(6);
+
+r.ellipse(r.width/2 + 20, r.height/2, 100, 100)
+.fill(255,255,255)
+.stroke(0,0,0)
+.strokeWidth(6);
+
+r.ellipse(r.width/2 - 50, r.height/2, 100, 100)
+.fill(255,255,255)
+.stroke(0,0,0)
+.strokeWidth(6);
+
+r.triangle(r.width/2 - 92, r.height/2 +30, r.width/2 +62, r.height/2 +30, r.width/2 +30, 550)
+.fill(0,0,0);
+
+r.rect(r.width/2 + 40, r.height/2 + 50, 10, 20)
+.fill(255,255,255);
+
+r.rect(r.width/2 + 25, r.height/2 + 50, 10, 30)
+.fill(255,255,255);
+
+
 
 r.draw();
-
-
